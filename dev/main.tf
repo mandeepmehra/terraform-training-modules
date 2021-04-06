dfe28f944dc7cc5709d4e5332d1bd7b08d011ea8
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-06 00:13:12 -0700

    Added ENV Tag for EC2 Creation

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,13 @@
 module "ec2" {
-  source = "../ec2module"
+  source  = "mandeepmehra/trainingec2module/aws"
+  version = "1.0.1"
   instanceCount = 1
+  env = "DEV"
+}
+
+
+module "s3"{
+  source = "../s3module"
+  bucket_name = "mybucket"
+  project_name = "ABC"
 }
